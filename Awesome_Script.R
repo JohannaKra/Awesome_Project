550488dd4468b2885092805b7fc8dc0cc6ec0e3b
JohannaKra <johanna.krastel@uni-bayreuth.de>
2025-09-18 14:32:52 +0200

    bla bla bla

--- Awesome_Script.R
+++ Awesome_Script.R
@@ -1,9 +1,13 @@
 # First Level: install renv and initiate the needed packages with it
 
-install.packages(renv)
+# install.packages(renv)
 
 renv::init()
 library(renv)
 renv::snapshot()
 
-# Second Level: share a whole Operating System 
+# Second Level: share a whole Operating System
+
+# this is changed
+
+# bla bla bla
---
+++ NewFile.R
@@ -0,0 +1,3 @@
+# This is a new file
+
+# bla bla bla
